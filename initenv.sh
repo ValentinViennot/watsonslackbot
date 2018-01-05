34aa8655f1f753d6891eb5038874501837cca51a
Valentin Viennot <valentin.viennot@ibm.com>
2018-01-05 15:52:46 +0100

    Init script

--- initenv.sh
+++ initenv.sh
@@ -3,4 +3,7 @@
 # New virtual env
 virtualenv watsonbot
 source watsonbot/bin/activate
-
+# Dependencies
+pip install watson-developer-cloud
+pip install slackclient
+pip install --upgrade google-api-python-client
